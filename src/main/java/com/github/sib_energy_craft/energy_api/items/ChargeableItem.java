8e607a37c0d4d6180754707dce0501989f195e4f
sibmaks <sibmaks@vk.com>
2024-04-11 16:52:51 +0300

    Improve items tooltips

--- src/main/java/com/github/sib_energy_craft/energy_api/items/ChargeableItem.java
+++ src/main/java/com/github/sib_energy_craft/energy_api/items/ChargeableItem.java
@@ -167,9 +167,17 @@ public interface ChargeableItem {
      */
     default void appendTooltip(@NotNull ItemStack itemStack,
                                @NotNull List<Text> tooltip) {
-        tooltip.add(Text.translatable("attribute.name.sib_energy_craft.charge", getCharge(itemStack))
+        var itemCharge = getCharge(itemStack)
+                .getAmount()
+                .stripTrailingZeros()
+                .toPlainString();
+        var maxCharge = getMaxCharge()
+                .getAmount()
+                .stripTrailingZeros()
+                .toPlainString();
+        tooltip.add(Text.translatable("attribute.name.sib_energy_craft.charge", itemCharge)
                 .setStyle(Style.EMPTY.withColor(Color.GRAY.getRGB())));
-        tooltip.add(Text.translatable("attribute.name.sib_energy_craft.max_charge", getMaxCharge())
+        tooltip.add(Text.translatable("attribute.name.sib_energy_craft.max_charge", maxCharge)
                 .setStyle(Style.EMPTY.withColor(Color.GRAY.getRGB())));
     }
 }
